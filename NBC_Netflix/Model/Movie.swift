5f1055dcd0c623db5723438f00618fb63e2ed88b
Jeon_Seong_gyu <junsung3807@naver.com>
2024-12-26 14:49:01 +0900

    ✨ Movie 모델객체 정의

---
+++ NBC_Netflix/Model/Movie.swift
@@ -0,0 +1,23 @@
+//
+//  Movie.swift
+//  NBC_Netflix
+//
+//  Created by 전성규 on 12/26/24.
+//
+
+import Foundation
+
+struct MovieResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int?
+    let title: String?
+    let posterPath: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title
+        case posterPath = "poster_path"
+    }
+}
